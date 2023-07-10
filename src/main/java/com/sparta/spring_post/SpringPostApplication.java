b51655abcaf225d4aa66ec9060b649ce539c0da6
dilution0216 <hglad0216@naver.com>
2023-07-10 20:44:35 +0900

    POST 3Layers 구성 후 CRUD 작성 ( dto, entity, Repository, Service, Controller ) 그리고 상세 주석 추가

---
+++ src/main/java/com/sparta/spring_post/SpringPostApplication.java
@@ -0,0 +1,16 @@
+package com.sparta.spring_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing              // 어플리케이션의 main method 가 있는 클래스에 적용해야함
+// JPA Auditing ( 감시, 감사 ) 기능을 활성화하기 위한 어노테이션
+public class SpringPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPostApplication.class, args);
+    }
+
+}
--- src/test/java/com/sparta/spring_post/SpringPostApplicationTests.java
+++ src/test/java/com/sparta/spring_post/SpringPostApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sparta.blog;
+package com.sparta.spring_post;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BlogApplicationTests {
+class SpringPostApplicationTests {
 
     @Test
     void contextLoads() {
